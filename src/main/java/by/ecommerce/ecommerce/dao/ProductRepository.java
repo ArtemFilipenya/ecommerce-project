2049c0c3eef37c15e63d11fc67eb8c43a8d025a6
Artem Filipenya <as.filipenya@gmail.com>
2023-04-28 10:49:05 +0300

    update config

--- src/main/java/by/ecommerce/ecommerce/dao/ProductRepository.java
+++ src/main/java/by/ecommerce/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package by.ecommerce.ecommerce.dao;
 
 import by.ecommerce.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
