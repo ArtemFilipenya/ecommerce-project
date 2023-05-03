b3a6800f8ae0a99b3a7b7e713515ccd873e5c77b
Artem Filipenya <as.filipenya@gmail.com>
2023-05-03 14:40:06 +0300

    update dao

--- src/main/java/by/ecommerce/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/by/ecommerce/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package by.ecommerce.ecommerce.dao;
 import by.ecommerce.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
